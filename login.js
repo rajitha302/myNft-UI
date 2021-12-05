1ed71d3d16e3affcb331d858aa76ba83db7adf63
rajitha302 <rajithadisanayaka999@gmail.com>
2021-12-05 14:20:29 +0530

    2 files modified, 1 added

---
+++ login.js
@@ -0,0 +1,5 @@
+const login = () => {
+  const email = document.getElementById('email').value;
+  const password = document.getElementById('pwd').value;
+  document.getElementById('uname').innerHTML = email;
+};
